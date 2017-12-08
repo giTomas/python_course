d2756a084709b9d0122b74d3f28fae82549c5042
MAIN\SK040122 <Tomas.Kosegi@anritsu.com>
2017-12-08 16:25:28 +0100

    news

--- exceptions/exception_raise.py
+++ exceptions/exception_raise.py
@@ -2,7 +2,20 @@
 
 class ShortInputException(Exception):
     '''A user-defined exception class'''
-    def __init__(self, length, atleast)
+    def __init__(self, length, atleast):
         Exception.__init__(self)
         self.length = length
         self.atleast = atleast
+
+try:
+    text = input("Enter something --> ")
+    if len(text) < 3:
+        raise ShortInputException(len(text), 3)
+except EOFError:
+    print('EOFError.')
+except ShortInputException as ex:
+    print(('ShortInputException: The input was ' +
+       '{0} long, expected at least {1}')
+      .format(ex.length, ex.atleast))
+else:
+    print('No exception was raised.')
---
+++ queques/queques.py
@@ -0,0 +1,5 @@
+# https://dbader.org/blog/queues-in-pyth
+from collections import deque
+
+# generate random numbers in some range
+# count hown many numbers are of upper and lower limit
